1564a9a5d6701981ba2863807573169bf5821364
YUEQI <xyq678@uw.edu>
2020-01-23 14:50:13 -0800

    complete chapter10 exercise 2 and 4

--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -3,60 +3,60 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
-
+employees <- paste("Employee", 1:100)
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
-
+salaries_2017 <- runif(100, 40000, 50000)
 
 # Create a vector of 100 annual salary adjustments between -5000 and 10000.
 # (A negative number represents a salary decrease due to corporate greed)
 # Again use the `runif()` function to pick 100 random numbers in that range.
-
+annual_salary_adjustment <- runif(100, -5000, 10000)
 
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employees, salaries_2017, annual_salary_adjustment, stringsAsFactors = FALSE)
 
 # Add a column to the `salaries` data frame that represents each person's
 # salary in 2018 (e.g., with the salary adjustment added in).
-
+salaries$salaries_2018 <- salaries_2017 + annual_salary_adjustment
 
 # Add a column to the `salaries` data frame that has a value of `TRUE` if the 
 # person got a raise (their salary went up)
-
-
+salaries$raise <- salaries$salaries_2018 > salaries_2017
 
 ### Retrieve values from your data frame to answer the following questions
 ### Note that you should get the value as specific as possible (e.g., a single
 ### cell rather than the whole row!)
 
 # What was the 2018 salary of Employee 57
-
+salaries[57, "salaries_2018"]
 
 # How many employees got a raise?
-
+nrow(salaries[salaries$raise == TRUE, ])
 
 # What was the dollar value of the highest raise?
-
+highest_raise <- max(salaries$annual_salary_adjustment)
 
 # What was the "name" of the employee who received the highest raise?
-
+salaries[salaries$annual_salary_adjustment == highest_raise, "employees"]
 
 # What was the largest decrease in salaries between the two years?
-
+largest_decrease <- min(salaries$annual_salary_adjustment)
 
 # What was the name of the employee who recieved largest decrease in salary?
-
+salaries[salaries$annual_salary_adjustment == largest_decrease, "employees"]
 
 # What was the average salary change?
-
+mean(salaries$annual_salary_adjustment)
 
 # For people who did not get a raise, how much money did they lose on average?
-
+mean(salaries[salaries$annual_salary_adjustment < 0, "annual_salary_adjustment"])
 
 ## Consider: do the above averages match what you expected them to be based on 
 ## how you generated the salaries?
 
-# Write a .csv file of your salary data to your working directory
 
+# Write a .csv file of your salary data to your working directory
+write.csv(salaries)
--- chapter-10-exercises/exercise-4/exercise.R
+++ chapter-10-exercises/exercise-4/exercise.R
@@ -4,36 +4,36 @@
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
+grants <- read.csv("data/gates_money.csv")
 
 # Use the View function to look at the loaded data
-
+View(grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-
+organization <- as.vector(grants$organization)
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
-
+is.vector(organization)
 
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-
+mean(grants$total_amount)
 
 # What was the dollar amount of the largest grant?
-
+largest_grant <- max(grants$total_amount)
 
 # What was the dollar amount of the smallest grant?
-
+smallest_grant <- min(grants$total_amount)
 
 # Which organization received the largest grant?
-
+grants[grants$total_amount == largest_grant, "organization"]
 
 # Which organization received the smallest grant?
-
+grants[grants$total_amount == smallest_grant, "organization"]
 
 # How many grants were awarded in 2010?
-
+nrow(grants[grants$start_year == 2010, ])
